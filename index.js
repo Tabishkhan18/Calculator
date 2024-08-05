76f1715f51b8183755dc6fcf9297a1626f134d09
Tabishkhan18 <tabishkhan1811@gmail.com>
2024-08-05 18:34:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let string = "";
+let isResult = false; // Track if the result has been evaluated
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+        try {
+            string = eval(string).toString();
+        }
+        catch{
+            string = "Error";
+        }
+        document.querySelector('input').value = string;
+        isResult = true; // Set flag to true when result is evaluated
+    }
+    else if(e.target.innerHTML == 'AC'){
+      string = ""
+      document.querySelector('input').value = string;
+    }
+    else if(e.target.innerHTML == 'X'){
+        string = string.slice(0, -1);
+        document.querySelector('input').value = string;
+    }
+
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+    }
+  })
+})
